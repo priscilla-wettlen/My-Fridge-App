f8b996dbdb80969183b934de832158c48a1ff397
Priscilla Silva <priscilla.sg91@gmail.com>
2022-08-30 22:06:10 +0200

    working on user profile

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Bio from './Bio';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Bio />
     </div>
   );
 }
