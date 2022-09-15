9ca983b585a44a29d2073c4e7b8aec69cd827bed
Priscilla Silva <priscilla.sg91@gmail.com>
2022-09-15 16:55:46 +0200

    added request button, need to add pop up

--- project-ts/src/components/Shelves/Card.tsx
+++ project-ts/src/components/Shelves/Card.tsx
@@ -11,10 +11,11 @@ type CardProps = {
 export const Card= (props: CardProps) => {
   return (
       <div className={styles.card}>
-        <img src={props.url} width={200} alt="" />
-        <h3 className={styles.item}>Item: {props.item}</h3>
-        <p className={styles.amnt}>Amount: {props.amount}</p>
-        <p className={styles.desc}>Description: {props.description}</p>
+        <img src={props.url} alt="" />
+        <h3 className={styles.item}>{props.item}</h3>
+        <p className={styles.amnt}>{props.amount}</p>
+        <p className={styles.desc}>{props.description}</p>
+        <button className={styles.requestItem}>Request Item</button>
       </div>
     
   )
--- project-ts/src/components/Shelves/ReadyToEat.tsx
+++ project-ts/src/components/Shelves/ReadyToEat.tsx
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import { Card } from './Card';
 import FoodItems from '../foodsAPI.json';
 import styles from './Shelf.module.css';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const ReadyToEat = () => {
+  const [currCard, setCurrCard] = useState(FoodItems[0].ReadyToEat.map(food =>
+    <Card url={food.url} item={food.item} amount={`${food.amount} pieces`} description={food.description} />
+  ).slice(0,2)
+  );
+  const HandleClickRight = () => {
+    setCurrCard(FoodItems[0].ReadyToEat.map(food => 
+              <Card url={food.url} item={food.item} amount={`${food.amount} pieces`} description={food.description} /> 
+          ).slice(0,2))
+  }
+  const HandleClickLeft = () => {
+    setCurrCard(FoodItems[0].ReadyToEat.map(food => 
+              <Card url={food.url} item={food.item} amount={`${food.amount} pieces`} description={food.description} /> 
+          ).slice(0,2))
+  }
   return (
     <section className={styles.shelf}>
       <h3 className={styles.sectionTitle}>Ready to Eat</h3>
         <div className={styles.container}>
-        {
-          FoodItems[0].ReadyToEat.map(food => 
-             <Card url={food.url} item={food.item} amount={`${food.amount} pieces`} description={food.description} />
-            )
-        }
+        <ArrowBackIosIcon className={styles.arrowBack} onClick={HandleClickLeft} />
+        {currCard}
+        <ArrowForwardIosIcon className={styles.arrowForward} onClick={HandleClickRight} />
       </div>
 
     </section>
